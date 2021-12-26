1550e595b53b032cd03c3148267c09ec22d2ae82
Sergio Lucas da Silva <sergiolucascaps@hotmail.com>
2021-12-25 23:23:32 -0300

    Persistindo com repositórios

---
+++ src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder
+                .Properties<string>()
+                .AreUnicode(false)
+                .HaveMaxLength(100);
+
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+
+namespace NerdStore.Catalogo.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _catalogoContext;
+
+        public ProdutoRepository(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+
+        public IUnitOfWork UnitOfWork => _catalogoContext;
+
+        public void Adicionar(Produto produto)
+            => _catalogoContext.Produtos.Add(produto);
+
+        public void Adicionar(Categoria categoria)
+            => _catalogoContext.Categorias.Add(categoria);
+
+        public void Atualizar(Produto produto)
+            => _catalogoContext.Produtos.Update(produto);
+
+        public void Atualizar(Categoria categoria)
+            => _catalogoContext.Categorias.Add(categoria);
+
+        public async Task<IEnumerable<Categoria>> ObterCategorias()
+            => await _catalogoContext.Categorias.AsNoTracking().ToListAsync();
+
+        public async Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+            => await _catalogoContext.Produtos.AsNoTracking().Include(p => p.Categoria).Where(p => p.Categoria.Codigo == codigo).ToListAsync();
+
+        public async Task<Produto> ObterPorId(Guid id)
+            => await _catalogoContext.Produtos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+            => await _catalogoContext.Produtos.AsNoTracking().ToListAsync();
+
+        public void Dispose()
+        {
+            _catalogoContext?.Dispose();
+        }
+    }
+}
