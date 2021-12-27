f14f0d0be2ac5f12fdd294016e728b609b18fa01
Sergio Lucas da Silva <sergiolucascaps@hotmail.com>
2021-12-26 23:35:28 -0300

    Implementando a camada de application

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Categoria, CategoriaViewModel>();
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(p => p.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(p => p.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(p => p.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+        }
+    }
+}
